8038f20b7324acc51b5efa8d2e476ae768509433
grabelnikov757 <grabelnikov757@gmail.com>
2024-08-26 08:15:10 +0300

    - Телепорты по кнопкам 1,2,3 - Примагнитить врагов Insert - Заморозить всех врагов Home - Размарозить всех врагов Delete - Забрать патроны 4

---
+++ MyGUI/Offsets.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+namespace MyGUI.Offsets
+{
+    public static class Offsets
+    {
+        public const Int32 BaseOffset = 0x0AA5EA0;
+        public static List<Player> AllPlayers = new List<Player>();
+    }
+
+    public struct Player
+    {
+        private const Int32 BaseOffset = 0x0AA5EA0;
+        private const Int32 _offset = 0x5A8;
+        public IntPtr NickName { get; set; }
+        public IntPtr Health { get; set; }
+        public IntPtr Position { get; set; }
+        public IntPtr MoveVector { get; set; }
+        public IntPtr AmmoInFirstWeapon { get; set; }
+        public IntPtr FaceVision { get; set; }
+        public float Distance { get; set; }
+        public float GetDistance(Player MainPlayer)
+        {
+            return Vector3.Distance(Program._swed.ReadVec(this.Position), Program._swed.ReadVec(MainPlayer.Position));
+        }
+        public Player()
+        {
+            this.NickName = Program._swed.ReadPointer(Program._moduleBase + BaseOffset)+(0x200 + (_offset * Offsets.AllPlayers.Count()));
+            this.Health = Program._swed.ReadPointer(Program._moduleBase + BaseOffset) + (0x0B8 + (_offset * Offsets.AllPlayers.Count()));
+            this.Position = Program._swed.ReadPointer(Program._moduleBase + BaseOffset) + (0x14 + (_offset * Offsets.AllPlayers.Count()));
+            this.MoveVector = Program._swed.ReadPointer(Program._moduleBase + BaseOffset) + (0x020 + (_offset * Offsets.AllPlayers.Count()));
+            this.AmmoInFirstWeapon = Program._swed.ReadPointer(Program._moduleBase + BaseOffset) + (0x180 + (_offset * Offsets.AllPlayers.Count()));
+            this.FaceVision = Program._swed.ReadPointer(Program._moduleBase + BaseOffset) + (0x098 + (_offset * Offsets.AllPlayers.Count()));
+            Offsets.AllPlayers.Add(this);
+        }
+    }
+
+
+}
--- MyGUI/Program.cs
+++ MyGUI/Program.cs
@@ -1,56 +1,335 @@
 ﻿using ClickableTransparentOverlay;
 using ImGuiNET;
+using MyGUI.Offsets;
 using Swed64;
+using System.Diagnostics;
 using System.Numerics;
+using System.Runtime.InteropServices;
+using _Offsets = MyGUI.Offsets.Offsets;
+using Point = SixLabors.ImageSharp.Point;
 namespace MyGUI
 {
     public class SavePoint
     {
-        public string title { get; set; } = "";
-        public Vector3 position { get; set; }
+        public string Title { get; set; } = "";
+        public Vector3 Position { get; set; }
     }
     public class Program : Overlay
     {
-        List<SavePoint> savePoints = new List<SavePoint>();
-        static public Swed swed = new Swed("quake3e-vulkan.x64");
-        static public IntPtr moduleBase = IntPtr.Zero;
-        static public IntPtr posAddress = IntPtr.Zero;
+        private static Vector3 FreezeFacePoin = new Vector3(87f, 0f, 0f);
+        public const string MyNick = "ZXClown";
+        public static int MyIndex { get; set; } = 0;
+        private static List<SavePoint> _savePoints = new List<SavePoint>();
+        public static Swed _swed = new Swed("quake3e-vulkan.x64");
+        public static IntPtr _moduleBase = IntPtr.Zero;
+        public static Player MyPlayer;
+        [STAThread]
         private static void Main(string[] args)
         {
-            moduleBase = swed.GetModuleBase("quake3e-vulkan.x64.exe");
-            posAddress = swed.ReadPointer(moduleBase, 0x0AA5EA0) + 0x5BC;
+            var hHook = SetHook();
+            Console.WriteLine("App start. Main process = " + MainProcess.Id);
+            _moduleBase = _swed.GetModuleBase("quake3e-vulkan.x64.exe");
             Program program = new Program();
             program.Start();
+            Application.Run();
+            Console.WriteLine("Unhook");
+            UnhookWindowsHookEx(hHook);
         }
         protected override void Render()
         {
             ImGui.Begin("MyGUI");
-            if (ImGui.Button("First button"))
+            while (_Offsets.AllPlayers.Count() < 10)
             {
-                SaveCurrentPosition();
+                Offsets.Player player = new Player();
             }
-            foreach (var point in savePoints)
+            if (ImGui.BeginMenu("Players") || true)
             {
-                if (ImGui.Button(point.title))
+                foreach (var player in _Offsets.AllPlayers.Select((Value, i) => new { i, Value }))
                 {
-                    TeleportToPosition(point.position);
+                    string nana = $"{_swed.ReadString(player.Value.NickName, 7)}";
+                    if (nana[0] == '\0') { continue; }
+                    ImGui.Separator();
+                    if (nana == MyNick)
+                    {
+                        MyIndex = player.i;
+                        MyPlayer = (Player)player.Value;
+                        ImGui.TextColored(new Vector4(1, 0, 1, 1), "It's me");
+                    }
+                    ImGui.Text($"Nick: {_swed.ReadString(player.Value.NickName, 7)}");
+                    ImGui.Text($"\tHealth: {_swed.ReadInt(player.Value.Health)} \tAmmo: {_swed.ReadInt(player.Value.AmmoInFirstWeapon)}\n\tPosition: {_swed.ReadVec(player.Value.Position)}\n\tMove: {_swed.ReadVec(player.Value.MoveVector)}");
+                    ImGui.Text($"\tLook: {_swed.ReadVec(player.Value.FaceVision)}");
+                    ImGui.Text($"\tDistance: {player.Value.GetDistance(_Offsets.AllPlayers[MyIndex])}");
                 }
             }
         }
-        private void TeleportToPosition(Vector3 position)
+        private static void TeleportToPosition(Vector3 position)
         {
-            swed.WriteVec(posAddress, position);
+            _swed.WriteVec(MyPlayer.Position, position);
         }
-        private void SaveCurrentPosition()
+        private static bool ListInitialize { get; set; } = false;
+
+        private static void InitializeList()
         {
-            Vector3 currentPlayerPosition = GetCurrentPlayerPosition();
-            string savePointTitle = "Save Point " + (savePoints.Count + 1);
-            SavePoint savePoint = new SavePoint { title = savePointTitle, position = currentPlayerPosition };
-            savePoints.Add(savePoint);
+            Vector3 currentPlayerPosition = _swed.ReadVec(MyPlayer.Position);
+            while (_savePoints.Count() <= 3)
+            {
+                SavePoint freeShit = new SavePoint { Position = currentPlayerPosition, Title = "Free slot" };
+                _savePoints.Add(freeShit);
+            }
+            ListInitialize = true;
         }
-        private Vector3 GetCurrentPlayerPosition()
+        private static void SaveCurrentPosition(int i)
         {
-            return swed.ReadVec(posAddress);
+            if (!ListInitialize)
+            {
+                InitializeList();
+            }
+
+            Vector3 currentPlayerPosition = _swed.ReadVec(MyPlayer.Position);
+            string savePointTitle = "Save Point " + (i + 1);
+            SavePoint savePoint = new SavePoint { Title = savePointTitle, Position = currentPlayerPosition };
+            if (_savePoints[i].Title == "Free slot")
+            {
+                _savePoints[i] = savePoint;
+            }
+            else
+            {
+                Console.WriteLine($"Teleport to {_savePoints[i].Position}");
+                TeleportToPosition(_savePoints[i].Position);
+            }
         }
+        #region DllImports
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, IntPtr lMod, int dwThreadId);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hHook);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("Kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(IntPtr lpModuleName);
+        [DllImport("user32.dll")]
+        private static extern IntPtr MessageBox(IntPtr hWnd, string text, string caption, int options);
+        [DllImport("user32.dll")]
+        private static extern void SetWindowText(IntPtr hWnd, string text);
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(ref Point lpPoint);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern void mouse_event(uint dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        #endregion
+        #region KeyBoardData
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProcDelegate _mCallback = LowLevelKeyboardHookProc;
+        private static IntPtr _mHHook;
+        #endregion
+        #region LowLevelKeyboardProcDelegate
+        private delegate IntPtr LowLevelKeyboardProcDelegate(int nCode, IntPtr wParam, IntPtr lParam);
+        #endregion
+        #region LowLevelKeyboardHookProc
+        private static IntPtr LowLevelKeyboardHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                var khs = ((KeyboardHookStruct)Marshal.PtrToStructure(lParam, typeof(KeyboardHookStruct)));
+                if (khs.VirtualKeyCode == 45) //Insert
+                {
+                    Task.Run(() => MagnetPlayers());
+                }
+                if (khs.VirtualKeyCode == 46) //Delete
+                {
+                    UnFreezePlayers();
+                }
+                if (khs.VirtualKeyCode == 36) //Home
+                {
+                    FreezePlayers();
+                }
+                if (khs.VirtualKeyCode == 97) // num1
+                {
+                    SaveCurrentPosition(0);
+                }
+                if (khs.VirtualKeyCode == 98) // num2
+                {
+                    SaveCurrentPosition(1);
+                }
+                if (khs.VirtualKeyCode == 99) // num3
+                {
+                    SaveCurrentPosition(2);
+                }
+                if (khs.VirtualKeyCode == 100) // num4
+                {
+                    Task.Run(() => ZeroAmmoToEnemy());
+                    //GodMode();
+                }
+            }
+            return CallNextHookEx(_mHHook, nCode, wParam, lParam);
+        }
+
+        #region ZeroAmmo
+        private static bool IsZeroAmmo { get; set; } = false;
+
+        private static async Task ZeroAmmoToEnemy()
+        {
+            IsZeroAmmo = !IsZeroAmmo;
+            while (IsZeroAmmo)
+            {
+                foreach (var player in _Offsets.AllPlayers)
+                {
+                    if (player.NickName == MyPlayer.NickName) { continue; }
+                    _swed.WriteInt(player.AmmoInFirstWeapon, 0);
+                }
+                await Task.Delay(50);
+            }
+        }
+        #endregion
+        #region GodMode
+        private static bool IsGodMode { get; set; } = false;
+        private static async Task GodMode()
+        {
+            IsGodMode = !IsGodMode;
+            while (IsGodMode)
+            {
+                _swed.WriteInt(MyPlayer.Health, 101);
+                await Task.Delay(50);
+            }
+        }
+        #endregion
+        #region Magnet
+        private static bool IsMagnet { get; set; } = false;
+        private static async Task MagnetPlayers()
+        {
+            IsMagnet = !IsMagnet;
+            while (IsMagnet)
+            {
+                Vector3 playerVector3 = _swed.ReadVec(MyPlayer.Position);
+                Vector3 playerVisioVector3 = _swed.ReadVec(MyPlayer.FaceVision);
+                if (playerVisioVector3.Y >= 0 && playerVisioVector3.Y <= 90)
+                {
+                    playerVector3.X = playerVector3.X + 100 + 100 * ((float)Math.Cos((double)playerVisioVector3.Y / 25 ));
+                    playerVector3.Y = playerVector3.Y + 100 + 100 * ((float)Math.Sin((double)playerVisioVector3.Y / 25 ));
+                }
+                else if (playerVisioVector3.Y <= -90 && playerVisioVector3.Y >= -180)
+                {
+                    playerVector3.X = playerVector3.X - 100 - 100 * ((float)Math.Cos((double)playerVisioVector3.Y /25 ));
+                    playerVector3.Y = playerVector3.Y - 100 - 100 * ((float)Math.Sin((double)playerVisioVector3.Y / 25 ));
+                }
+                else if (playerVisioVector3.Y >= 90 && playerVisioVector3.Y <= 180)
+                {
+                    playerVector3.X = playerVector3.X - 100 - 100 * ((float)Math.Cos((double)playerVisioVector3.Y / 25 ));
+                    playerVector3.Y = playerVector3.Y + 100 + 100 * ((float)Math.Sin((double)playerVisioVector3.Y / 25 ));
+                }
+                else if (playerVisioVector3.Y <= 0 && playerVisioVector3.Y >= -90)
+                {
+                    playerVector3.X = playerVector3.X + 100 + 100 * ((float)Math.Cos((double)playerVisioVector3.Y / 25 ));
+                    playerVector3.Y = playerVector3.Y - 100 - 100 * ((float)Math.Sin((double)playerVisioVector3.Y / 25 ));
+                }
+                foreach (var player in _Offsets.AllPlayers)
+                {
+                    if (player.NickName == MyPlayer.NickName) { continue; }
+                    _swed.WriteVec(player.Position, playerVector3);
+                }
+            }
+        }
+        #endregion
+        #region Freeze 
+        private static bool IsFreeze { get; set; } = false;
+        private static bool FreezeStart { get; set; } = false;
+
+        private static void UnFreezePlayers()
+        {
+            if (IsFreeze)
+            {
+                Task.Run(() => Freezing(false));
+            }
+        }
+        private static void FreezePlayers()
+        {
+            if (!IsFreeze)
+            {
+                Task.Run(() => Freezing(true));
+            }
+        }
+        private static async Task Freezing(bool enable)
+        {
+            FreezeStart = true;
+            IsFreeze = enable;
+            if (enable)
+            {
+                Dictionary<Player, Vector3> FreezePositions = new Dictionary<Player, Vector3>();
+                foreach (Player player in _Offsets.AllPlayers)
+                {
+                    if (player.NickName == MyPlayer.NickName)
+                    {
+                        continue;
+                    }
+                    Vector3 freezePosition;
+                    freezePosition = _swed.ReadVec(player.Position);
+                    FreezePositions.Add(player, freezePosition);
+                }
+                while (IsFreeze)
+                {
+                    foreach (var freezePosition in FreezePositions)
+                    {
+                        _swed.WriteVec(freezePosition.Key.Position, freezePosition.Value);
+                        _swed.WriteVec(freezePosition.Key.FaceVision, FreezeFacePoin);
+                    }
+                }
+            }
+        }
+        #endregion
+        #endregion
+        #region SetHook
+        public static nint SetHook()
+        {
+            try
+            {
+                using (Process curProcess = Process.GetCurrentProcess())
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    return SetWindowsHookEx(WH_KEYBOARD_LL, _mCallback, GetModuleHandle(IntPtr.Zero), 0);
+                }
+            }
+            catch
+            {
+                return nint.Zero;
+            }
+        }
+        #endregion
+        #region KeyboardHookStruct
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KeyboardHookStruct
+        {
+            public readonly int VirtualKeyCode;
+            public readonly int ScanCode;
+            public readonly int Flags;
+            public readonly int Time;
+            public readonly IntPtr ExtraInfo;
+        }
+        #endregion
+        #region Params
+        private static bool IsStart { get; set; } = false;
+        private static IntPtr Descriptor { get; set; } = 0;
+        private static Point MousePosition { get; set; }
+        private static Point MouseLastIterationPosition { get; set; }
+        private static bool MainProcessIsActive { get; set; } = false;
+        private static Task MainProcess
+        {
+            get
+            {
+                lock (StartProcess())
+                    return Task.FromResult(Process.GetCurrentProcess());
+            }
+        }
+        #endregion
+        #region MainLogic
+        private static async Task StartProcess()
+        {
+            if (MainProcessIsActive) { return; }
+            MainProcessIsActive = true;
+            //while (true)
+            //{
+            //    await Task.Delay(10);
+            //}
+        }
+        #endregion
     }
 }
